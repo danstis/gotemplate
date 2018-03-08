19dfeb791bc2b46177c91ba662ea257d7faac38d
Dan Anstis <dan@bsod.co.nz>
2018-03-08 12:43:09 +1000

    Update

--- main.go
+++ main.go
@@ -4,9 +4,9 @@ import (
 	"log"
 )
 
-const VERSION = "0.0.0-default"
+var Version = "0.0.0-default"
 
 // Main entry point for the app.
 func main() {
-	log.Printf("Version %q", VERSION)
+	log.Printf("Version %q", Version)
 }
